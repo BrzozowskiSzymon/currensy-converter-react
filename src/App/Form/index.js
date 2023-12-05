4a24718f3046f473f3dc429ca9a9e5f1bd18ea47
Szymon Brzozowski <brzozowski.szymon.98@gmail.com>
2023-12-06 00:48:18 +0100

    essa

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -13,64 +13,64 @@ export const Form = ({ calculateResult, result }) => {
     }
 
     return (
-        <form 
-        className="form" 
-        onSubmit={onSubmit}>
-        <h1 
-        className="container__header">
+        <h1
+            className="container__header">
             Kantor Online
-        </h1>
-        <p>
-            <label>
-                <span className="form__labelText">
-                    Kwota zł :
-                </span>
-                <input 
-                value={amount}
-                onChange={({ target}) => setAmount(target.value)}
-                placeholder="Wpisz kwote" 
-                className="form__field" 
-                type="number" 
-                required 
-                step="0.01"
-                min="1"
-                />
-            </label>
-        </p>
-        <p>
-            <label>
-                <span className="form__labelText">
-                    Waluta :
-                </span>
-                <select 
-                    className="form__field"
-                    value={currency}
-                    onChange={({ target }) => setCurrency(target.value)}
-                >
-                {currencies.map((currency => (
-                    <option
-                    key={currency.short}
-                    value={currency.short}
+        </h1>,
+        <form
+            className="form"
+            onSubmit={onSubmit}>
+            <p>
+                <label>
+                    <span className="form__labelText">
+                        Kwota zł :
+                    </span>
+                    <input
+                        value={amount}
+                        onChange={({ target }) => setAmount(target.value)}
+                        placeholder="Wpisz kwote"
+                        className="form__field"
+                        type="number"
+                        required
+                        step="0.01"
+                        min="1"
+                    />
+                </label>
+            </p>
+            <p>
+                <label>
+                    <span className="form__labelText">
+                        Waluta :
+                    </span>
+                    <select
+                        className="form__field"
+                        value={currency}
+                        onChange={({ target }) => setCurrency(target.value)}
                     >
-                    {currency.name}
-                    </option>
-                )))}
-                </select>
-            </label>
-        </p>
-        <p>
-            <button 
-            className="form__button">
-                <span>Przelicz </span>
-            </button>
-        </p>
-        <p className="form__info">
-            Aktalne kursy walut ze strony nbp.pl z dnia 2023-08-16
-        </p>
-        <p className="form__result"><strong></strong>
-        </p>
-    
-    <Result result={result}/>
-    </form >
+                        {currencies.map((currency => (
+                            <option
+                                key={currency.short}
+                                value={currency.short}
+                            >
+                                {currency.name}
+                            </option>
+                        )))}
+                    </select>
+                </label>
+            </p>
+            <p>
+                <button
+                    className="form__button">
+                    <span>Przelicz </span>
+                </button>
+            </p>
+            <p className="form__info">
+                Aktalne kursy walut ze strony nbp.pl z dnia 2023-08-16
+            </p>
+            <p className="form__result"><strong></strong>
+            </p>
+
+            <Result result={result} />
+        </form >
     );
 };
